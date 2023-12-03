e3bdaf80320c2ee1adfa65b82591f5417a7dff86
Leamea <agotalea@gmail.com>
2023-12-03 19:49:34 +0100

    CRUD of SpeciesLogic implemented

---
+++ DRPDH3_HFT_2023241.Logic/SpeciesLogic.cs
@@ -0,0 +1,42 @@
+﻿using DRPDH3_HFT_2023241.Models;
+using DRPDH3_HFT_2023241.Repository;
+using System;
+using System.Linq;
+
+namespace DRPDH3_HFT_2023241.Logic
+{
+    public class SpeciesLogic
+    {
+        IRepository<Species> repo;
+
+        public SpeciesLogic(IRepository<Species> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Species item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Species Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Species> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Species item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
