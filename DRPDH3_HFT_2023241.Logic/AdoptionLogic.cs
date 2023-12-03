9ae832636517279cf5396db0ea6951ced47c4896
Leamea <agotalea@gmail.com>
2023-12-03 19:54:02 +0100

    CRUD of AdoptionLogic implemented.

---
+++ DRPDH3_HFT_2023241.Logic/AdoptionLogic.cs
@@ -0,0 +1,45 @@
+﻿using DRPDH3_HFT_2023241.Models;
+using DRPDH3_HFT_2023241.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRPDH3_HFT_2023241.Logic
+{
+    public class AdoptionLogic
+    {
+        IRepository<Adoption> repo;
+
+        public AdoptionLogic(IRepository<Adoption> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Adoption item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Adoption Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Adoption> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Adoption item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
