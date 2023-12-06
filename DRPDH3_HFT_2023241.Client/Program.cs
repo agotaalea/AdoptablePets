778c8eb3275af5043967cafe2de3203789a494b2
Leamea <agotalea@gmail.com>
2023-12-06 23:36:47 +0100

    Init RestService

--- DRPDH3_HFT_2023241.Client/Program.cs
+++ DRPDH3_HFT_2023241.Client/Program.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using DRPDH3_HFT_2023241.Models;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace DRPDH3_HFT_2023241.Client
 {
     internal class Program
     {
+        static RestService rest;
+
         static void Main(string[] args)
         {
+            rest = new RestService("http://localhost:47912/", "pet");
         }
     }
 }
